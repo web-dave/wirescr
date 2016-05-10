a25a2befcb181f97b6fa2c46202df51ce742273d
damu1de <david.muellerchen@acando.de>
2016-05-10 08:06:54 +0200

    ignore json

--- index.js
+++ index.js
@@ -22,26 +22,27 @@ const handleError = (message) => {
     process.exit(1);
 };
 
-function fromDir(startPath,filter){
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
+function fromDir(startPath, filter) {
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
         return;
     }
 
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=(path.join(startPath,files[i])).replace(/\\/g, "/");
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = (path.join(startPath, files[i])).replace(/\\/g, "/");
         var stat = fs.lstatSync(filename);
-        
-        if (stat.isDirectory()){
-            fromDir(filename,filter);
-        }
-        else if (filename.indexOf(filter)>=0) {
-            
-            var relPath = path.relative(program.root,filename).replace(/\\/g, "/");
-            switch(filter){
+
+        if (stat.isDirectory()) {
+            fromDir(filename, filter);
+        } else if (filename.indexOf(filter) >= 0) {
+
+            var relPath = path.relative(program.root, filename).replace(/\\/g, "/");
+            switch (filter) {
                 case '.js':
-                    jsFiles += `<script src="${relPath}"></script>\n`;
+                    if (filename.indexOf('.json') === -1) {
+                        jsFiles += `<script src="${relPath}"></script>\n`;
+                    }
                     break;
                 case '.css':
                     cssFiles += `<link rel="stylesheet" href="${relPath}">\n`;
@@ -57,49 +58,45 @@ const getFullPath = (file) => {
 };
 
 program
-.version('1.0.0')
-.option('-i, --input <input>', 'Input file')
-.option('-o, --output <output>', 'Output file (defaults to input when omitted)')
-.option('-c, --css <css>', 'css file(s) to inject (file or directory)')
-.option('-j, --js <js>', 'js file(s) to inject (file or directory)')
-.option('-r, --root <root>', 'src root')
-.parse(process.argv);
+    .version('1.0.0')
+    .option('-i, --input <input>', 'Input file')
+    .option('-o, --output <output>', 'Output file (defaults to input when omitted)')
+    .option('-c, --css <css>', 'css file(s) to inject (file or directory)')
+    .option('-j, --js <js>', 'js file(s) to inject (file or directory)')
+    .option('-r, --root <root>', 'src root')
+    .parse(process.argv);
 
-if(!program.input) {
+if (!program.input) {
     handleError('Please specify an input file');
-}
-else {
+} else {
     try {
         program.input = getFullPath(program.input);
 
-        if(fs.lstatSync(program.input).isDirectory()) {
+        if (fs.lstatSync(program.input).isDirectory()) {
             handleError(`'${program.input}' is a directory, please specify an input file`);
         }
-        if(!fs.lstatSync(program.input).isFile()) {
+        if (!fs.lstatSync(program.input).isFile()) {
             handleError(`File '${program.input}' not found`);
         }
-    }
-    catch(e) {
+    } catch (e) {
         handleError(`File '${program.input}' not found`);
     }
 }
 
 program.output = program.output ? getFullPath(program.output) : program.input;
 
-if(program.root) {
+if (program.root) {
     const p = program.root;
 
     try {
-        fromDir(p,'.js');
-    }
-    catch(e) {
+        fromDir(p, '.js');
+    } catch (e) {
         handleError(`File or folder '${p}' not found`);
     }
 
     try {
-        fromDir(p,'.css');
-    }
-    catch(e) {
+        fromDir(p, '.css');
+    } catch (e) {
         handleError(`File or folder '${p}' not found`);
     }
 }
@@ -109,6 +106,6 @@ const replaceJs = replaceFunc(jsFiles);
 const replaceCss = replaceFunc(cssFiles);
 
 fs.createReadStream(program.input)
-.pipe(replaceStream(/(<!\-\- inject:js \-\->)([\s\S]*?)(<!\-\- endinject \-\->)/gm, replaceJs))
-.pipe(replaceStream(/(<!\-\- inject:css \-\->)([\s\S]*?)(<!\-\- endinject \-\->)/gm, replaceCss))
-.pipe(fs.createWriteStream(program.output));
+    .pipe(replaceStream(/(<!\-\- inject:js \-\->)([\s\S]*?)(<!\-\- endinject \-\->)/gm, replaceJs))
+    .pipe(replaceStream(/(<!\-\- inject:css \-\->)([\s\S]*?)(<!\-\- endinject \-\->)/gm, replaceCss))
+    .pipe(fs.createWriteStream(program.output));
